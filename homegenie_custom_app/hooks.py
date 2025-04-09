c2fa8c89367a9716bc238d74f08cc5a6522b9f3c
arunadithyat <arunadithyat@gmail.com>
2025-04-10 01:07:24 +0530

    KAC

--- homegenie_custom_app/hooks.py
+++ homegenie_custom_app/hooks.py
@@ -30,18 +30,18 @@ app_license = "MIT"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-doctype_js = {
-    "Customer" : "public/customer/customer.js",
-    "Maintenance Visit":"public/customer/customer.js",
-    "Event":"public/event/event.js",
-    "Issue":"public/customer/customer.js",
-    "Company":"public/js/company.js",
-    "Sales Invoice":"public/js/sales_invoice.js",
-    "Installation Note":"public/js/installation_note.js",
-    "Customer": "public/js/customer.js",
-    "Job Applicant": "public/js/job_applicant.js",
-    "Razorpay Settings": "public/js/razorpay_setting.js",
-    }
+# doctype_js = {
+#     "Customer" : "public/customer/customer.js",
+#     "Maintenance Visit":"public/customer/customer.js",
+#     "Event":"public/event/event.js",
+#     "Issue":"public/customer/customer.js",
+#     "Company":"public/js/company.js",
+#     "Sales Invoice":"public/js/sales_invoice.js",
+#     "Installation Note":"public/js/installation_note.js",
+#     "Customer": "public/js/customer.js",
+#     "Job Applicant": "public/js/job_applicant.js",
+#     "Razorpay Settings": "public/js/razorpay_setting.js",
+#     }
    
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
@@ -56,7 +56,14 @@ doctype_js = {
     # {"doctype":"Client Script","filters":[["module", "=", "Homegenie"]]}
 
 
-fixtures = ["Custom Field","Property Setter"]
+fixtures = [
+    {
+        "dt": "Custom Field",
+        "filters": [
+            ["name", "=", "custom_kac"] 
+        ]
+    }
+]
 
 
 # Home Pages
@@ -143,25 +150,25 @@ fixtures = ["Custom Field","Property Setter"]
 # ---------------
 # Hook on document methods and events
 
-doc_events = {
-	"Quotation": {
-        "on_submit":"homegenie_custom_app.homegenie.doctype.installation_request.installation_request.get_connection_of_quo_to_ir"
-	},
-    "Installation Note":{
-        "on_submit":"homegenie_custom_app.homegenie.api.get_connection_of_si_to_ins",
-        "on_change":"homegenie_custom_app.homegenie.api.technician_validation"
-    }
-}
+# doc_events = {
+# 	"Quotation": {
+#         "on_submit":"homegenie_custom_app.homegenie.doctype.installation_request.installation_request.get_connection_of_quo_to_ir"
+# 	},
+#     "Installation Note":{
+#         "on_submit":"homegenie_custom_app.homegenie.api.get_connection_of_si_to_ins",
+#         "on_change":"homegenie_custom_app.homegenie.api.technician_validation"
+#     }
+# }
 
-# Scheduled Tasks
-# ---------------
-scheduler_events = {
-	"cron": {
-		"*/30 * * * *" : [
-			"homegenie_custom_app.homegenie.api.shift_sync_checkin"
-		]
-    }
-}
+# # Scheduled Tasks
+# # ---------------
+# scheduler_events = {
+# 	"cron": {
+# 		"*/30 * * * *" : [
+# 			"homegenie_custom_app.homegenie.api.shift_sync_checkin"
+# 		]
+#     }
+# }
 # scheduler_events = {
 #	"all": [
 #		"homegenie_custom_app.tasks.all"
@@ -206,7 +213,7 @@ scheduler_events = {
 # Ignore links to specified DocTypes when deleting documents
 # -----------------------------------------------------------
 
-ignore_links_on_delete = ["Quotation","Installation Request"]
+# ignore_links_on_delete = ["Quotation","Installation Request"]
 # ignore_links_on_cancel = ["Quotation","Installation Request"]
 
 # Request Events
